43cadf4794e9f2cf9983dd4f7e93089b9fe17333
Omar Ouhra <omarmaroc26@gmail.com>
2025-03-01 14:28:06 +0000

    feat: header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,13 @@ export default function Home() {
   return (
     <section>
       {/* Header */}
-      <div></div>
+      <section className="flex items-center justify-between space-x-3 py-3">
+        <hr className="h-px w-full text-[#F2F2F2]" />
+        <p className="text-[#758084] whitespace-nowrap uppercase text-xs font-mono">
+          Noise chapter 2
+        </p>
+        <hr className="h-px w-full text-[#F2F2F2]" />
+      </section>
 
       {/* Nose Features */}
       <section></section>
